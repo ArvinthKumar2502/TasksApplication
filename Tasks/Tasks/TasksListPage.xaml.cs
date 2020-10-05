1724101f1d89efcd80efd9bc7dcdedb45b7d8ebc
Arvinth Kumar <arvinthkumar.s@gmail.com>
2020-10-05 17:16:53 +0530

    commit 1

---
+++ Tasks/Tasks/TasksListPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tasks
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TasksListPage : ContentPage
+    {
+        public TasksListPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
